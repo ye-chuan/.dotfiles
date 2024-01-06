b5b9c5e7f48ea9cf02837b512adb684ba77700de
ye-chuan <76342787+ye-chuan@users.noreply.github.com>
2024-01-07 01:16:28 +0800

    WezTerm: Added key mappings
    Mappings for multiplexing made to mimick tmux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,15 +11,16 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = "Catppuccin Mocha"
-config.hide_tab_bar_if_only_one_tab = true
--- config.use_fancy_tab_bar = false -- Use native tabs instead of ascii tabs
+config.enable_tab_bar = false
+-- config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false -- Use native tabs instead of ascii tabs
 config.window_background_opacity = 0.9
 config.scrollback_lines = 3500
 
 config.set_environment_variables = {
   -- Environmental variables can be set directly in WezTerm here.
   -- e.g. cmd.exe uses the %PROMPT% environmental variable to decide
-  -- what to use as the prompt. 
+  -- what to use as the prompt.
   -- The $P $_ etc. special codes are described in `prompt /?`
   -- $E is the ESC character used to send escape sequences to the terminal
   -- Terminal text colours are set via a Control Sequence Introducer (CSI) sequence
@@ -41,5 +42,209 @@ config.set_environment_variables = {
   -- can configure this hacky way of emitting as OSC 7 everytime it prints the prompt.
   prompt = '$E]7;file://localhost/$P$E\\$E[32m$T$E[0m $E[35m$P$E[36m$_$G$E[0m ',
 }
+
+-- Keymaps
+config.disable_default_key_bindings = true
+config.leader = { key = "\\", mods = "CTRL", timeout_milliseconds = 1000 }  -- Will be used for multiplexing like tmux
+config.keys = {
+    -- Clipboard
+    {
+        key = "y",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.CopyTo("Clipboard"),
+    },
+    {
+        key = "p",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.PasteFrom("Clipboard"),
+    },
+    -- Window
+    {
+        key = "Enter",
+        mods = "ALT",
+        action = wezterm.action.ToggleFullScreen,
+    },
+    -- Visuals
+    {
+        key = "-",
+        mods = "CTRL",
+        action = wezterm.action.DecreaseFontSize,
+    },
+    {
+        key = "=",
+        mods = "CTRL",
+        action = wezterm.action.IncreaseFontSize,
+    },
+    -- Scroll
+    {
+        key = "u",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.ScrollByPage(-0.5),
+    },
+    {
+        key = "d",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.ScrollByPage(0.5),
+    },
+    {
+        key = "g",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.ScrollToTop,
+    },
+    -- Text
+    {
+        key = "v",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.ActivateCopyMode,
+    },
+    {
+        key = "c",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.QuickSelect,
+    },
+    {
+        key = "?",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.Search("CurrentSelectionOrEmptyString"),
+    },
+    -- Others
+    {
+        key = ":",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.ActivateCommandPalette,
+    },
+
+    ---- Multiplexer (will use mappings very similar to tmux for easy transition next time)
+    --- Panes
+    {
+        key = "s",  -- Apparently Vim and WezTerm has different definitions of split horizontal...
+        mods = "LEADER",
+        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+    },
+    {
+        key = "v",
+        mods = "LEADER",
+        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+    {
+        key = "h",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection("Left"),
+    },
+    {
+        key = "l",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection("Right"),
+    },
+    {
+        key = "j",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection("Down"),
+    },
+    {
+        key = "k",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection("Up"),
+    },
+    {
+        key = "l",
+        mods = "LEADER",
+        action = wezterm.action.ActivateLastTab,
+    },
+    {
+        key = "q",
+        mods = "LEADER",
+        action = wezterm.action.PaneSelect({ mode = "Activate" }),
+    },
+    {
+        key = "x",
+        mods = "LEADER",
+        action = wezterm.action.CloseCurrentPane({ confirm = true }),
+        -- No confirmation for what WezTerm deems stateless (see `config.skip_close_confirmation_for_processes_named`)
+    },
+    {
+        key = "!",
+        mods = "LEADER|SHIFT",
+        action = wezterm.action_callback(function(win, pane)    -- action_callback helps create an event and hook it to this a callback with `wezterm.on(event_name, callback)`
+            local tab, window = pane:move_to_new_tab()
+            tab:activate()
+        end),
+    },
+    {
+        key = "z",
+        mods = "LEADER",
+        action = wezterm.action.TogglePaneZoomState,
+    },
+    {
+        key = "LeftArrow",
+        mods = "LEADER",
+        action = wezterm.action.AdjustPaneSize({"Left", 1}),
+    },
+    {
+        key = "RightArrow",
+        mods = "LEADER",
+        action = wezterm.action.AdjustPaneSize({"Right", 1}),
+    },
+    {
+        key = "UpArrow",
+        mods = "LEADER",
+        action = wezterm.action.AdjustPaneSize({"Up", 1}),
+    },
+    {
+        key = "DownArrow",
+        mods = "LEADER",
+        action = wezterm.action.AdjustPaneSize({"Down", 1}),
+    },
+    --- Tabs (tmux windows)
+    {
+        key = "c",
+        mods = "LEADER",
+        action = wezterm.action.SpawnTab( "CurrentPaneDomain" ),
+    },
+    {
+        key = "&",
+        mods = "LEADER|SHIFT",
+        action = wezterm.action.CloseCurrentTab({ confirm = true }),
+    },
+    {
+        key = ",",
+        mods = "LEADER",
+        action = wezterm.action.PromptInputLine({   -- Prompt for new name
+            description = "Rename Tab",
+            action = wezterm.action_callback(function(window, pane, line)
+                if line then    -- If line is not `nil` (meaning didn't press ESC)
+                    window:active_tab():set_title(line) -- If line is "" then default name will be used
+                end
+            end),
+        }),
+    },
+    -- Navigation
+    {
+        key = "w",
+        mods = "LEADER",
+        action = wezterm.action.ShowTabNavigator,
+    },
+    {
+        key = "n",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTabRelative(1),
+    },
+    {
+        key = "p",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTabRelative(-1),
+    },
+    { key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
+    { key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
+    { key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
+    { key = "4", mods = "LEADER", action = wezterm.action.ActivateTab(3) },
+    { key = "5", mods = "LEADER", action = wezterm.action.ActivateTab(4) },
+    { key = "6", mods = "LEADER", action = wezterm.action.ActivateTab(5) },
+    { key = "7", mods = "LEADER", action = wezterm.action.ActivateTab(6) },
+    { key = "8", mods = "LEADER", action = wezterm.action.ActivateTab(7) },
+    { key = "9", mods = "LEADER", action = wezterm.action.ActivateTab(8) },
+    { key = "0", mods = "LEADER", action = wezterm.action.ActivateTab(9) },
+}
+
 -- and finally, return the configuration to wezterm
 return config
