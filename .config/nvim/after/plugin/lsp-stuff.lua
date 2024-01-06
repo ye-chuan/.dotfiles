db6841a02e5fcce780d99d76cea5eb45781fc10c
ye-chuan <76342787+ye-chuan@users.noreply.github.com>
2024-01-07 03:10:52 +0800

    NeoVim: Added mason-lspconfig plugin
    Mainly to add the ensure_installed list of LSPs

--- .config/nvim/after/plugin/lsp-stuff.lua
+++ .config/nvim/after/plugin/lsp-stuff.lua
@@ -1,6 +1,6 @@
----- Must be setup in the order: Mason -> LSP-Config (because Mason temporarily adds the LSP locations to the PATH for LSP-Config?)
+---- Must be setup in the order: Mason -> Mason-LSPConfig -> LSP-Config (because Mason temporarily adds the LSP locations to the PATH for LSP-Config?)
 
--- Packages / LSPs are installed in /nvim-data/mason
+-- Packages / LSPs are installed to ~/.local/share/nvim/mason (Windows: %LOCALAPPDATA%/nvim-data/mason)
 require("mason").setup({
     -- See :h mason-settings
     ui = {
@@ -20,16 +20,42 @@ require("mason").setup({
     }
 })
 
+-- Mason-LSPConfig is an optional bridge between the 2, currently mainly using it to ensure_installed
+-- Note that it uses LSPConfig server names instead of the package names in Mason (see `:h mason-lspconfig-server-map`)
+require("mason-lspconfig").setup({
+    -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "lua_ls" }
+    -- This setting has no relation with the `automatic_installation` setting.
+    ---@type string[]
+    ensure_installed = {
+        "clangd",
+        "pyright",
+        "jdtls",
+        "tsserver",
+        "html",
+    },
+
+    -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+    -- This setting has no relation with the `ensure_installed` setting.
+    -- Can either be:
+    --   - false: Servers are not automatically installed.
+    --   - true: All servers set up via lspconfig are automatically installed.
+    --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
+    --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+    ---@type boolean
+    automatic_installation = false,
+})
 
 ----- Setup / Configure Each LSP Server
 local nvim_cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()    -- Remember to broacast the extended capabilites that nvim-cmp provided to NeoVim to all the LSP Servers (these includes snippet support, auto-imports etc.)
 
 -- see :h lspconfig-all for recommended configs
-local lspconfig = require('lspconfig')
-lspconfig.clangd.setup {
+local lspconfig = require("lspconfig")
+local clangd_setup_table = {
     capabilities = nvim_cmp_capabilities,
+}
+if vim.fn.has("win32") then
     -- So for clangd to work with mingw gcc in Windows, a few things need to be done
-    cmd = {"clangd", "--query-driver=C:/_Software/msys64/mingw64/bin/gcc.exe"}, -- First is to run clangd with the flag `--query-driver {path-to-gcc}` to **allow it** to query the gcc compiler for standard includes and stuff
+    clangd_setup_table["cmd"] = {"clangd", "--query-driver=C:/_Software/msys64/mingw64/bin/gcc.exe"}    -- First is to run clangd with the flag `--query-driver {path-to-gcc}` to **allow it** to query the gcc compiler for standard includes and stuff
     -- Very important to note that we are merely allowing clangd to query, but it will not query unless it know it has to, which is where step 2 comes in: we need to let clangd know how to compile how source,
     -- in other words what is the compilation command used to compile (and consequently what is the compiler used to compile, if we specify that we are compiling with gcc, and if we gave it permission to query it (earlier),
     -- then it will query gcc (this will solve the issues like "<stdio.h> not found".
@@ -51,24 +77,27 @@ lspconfig.clangd.setup {
     --        Remove: -W*                     # strip all other warning-related flags
     --        Compiler: gcc                   # Change argv[0] of compile flags to `gcc`
     --      ```
-}
+end
+lspconfig.clangd.setup(clangd_setup_table)
 
-lspconfig.jdtls.setup{
+lspconfig.pyright.setup({
+    capabilities = nvim_cmp_capabilities,
+})
+
+lspconfig.jdtls.setup({
     capabilities = nvim_cmp_capabilities,
     root_dir = function(filename)
         return vim.fs.dirname(vim.fs.find({".projectroot", ".git"}, {upward = true})[1])
     end,
-    }
-lspconfig.pyright.setup{
-    capabilities = nvim_cmp_capabilities,
-}
+})
 
-lspconfig.html.setup{
+lspconfig.tsserver.setup({
     capabilities = nvim_cmp_capabilities,
-}
+})
 
-lspconfig.tsserver.setup{
-}
+lspconfig.html.setup({
+    capabilities = nvim_cmp_capabilities,
+})
 
 ----- COSMETICS -----
 local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
@@ -80,37 +109,37 @@ end
 ----- KEY MAPPINGS -----
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
--- vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)     -- Goto prev error
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)     -- Goto next error
-vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist)
+-- vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)     -- Goto prev error
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)     -- Goto next error
+vim.keymap.set("n", "<Leader>q", vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to map the following keys ONLY
 -- AFTER The language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {      -- Attach this autocmd to the built-in event "LspAttach" (see :he LspAttach)
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),     -- Creates a new group for these autocmds called "UserLspConfig
+vim.api.nvim_create_autocmd("LspAttach", {      -- Attach this autocmd to the built-in event "LspAttach" (see :he LspAttach)
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),     -- Creates a new group for these autocmds called "UserLspConfig
   callback = function(ev)   -- ev is the event, which has an attribute .buf that is the current buffer number
     -- Enable completion triggered by <C-X><C-O> (currently disabled for auto-completion plugin instead)
-    -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'  -- vim.bo[ev.buf] indexes and returns current buffer, then override/set it's omnifunc (see :he omnifunc) with the one in our lsp
+    -- vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"  -- vim.bo[ev.buf] indexes and returns current buffer, then override/set it's omnifunc (see :he omnifunc) with the one in our lsp
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }    -- Store current buffer no. (ie. 0) so mappings are only set for this buffer
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    -- vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-    -- vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    -- vim.keymap.set('n', '<Leader>wl', function()
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    -- vim.keymap.set("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+    -- vim.keymap.set("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    -- vim.keymap.set("n", "<Leader>wl", function()
     --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     -- end, opts)
-    vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    -- vim.keymap.set('n', '<Leader>f', function()
+    vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, opts)
+    -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    -- vim.keymap.set("n", "<Leader>f", function()
     --   vim.lsp.buf.format { async = true }
     -- end, opts)
   end,
