c2483e014fcd2eb12d948d3d9f8e18e73d792e44
ye-chuan <76342787+ye-chuan@users.noreply.github.com>
2024-01-27 01:06:47 +0800

    Bash: Updated installation script to be interactive

--- .config/scripts/apt_install.sh
+++ .config/scripts/apt_install.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
-# A list of apps to install on a new machine
+# A list of apps to install on a new machine (this script might not be portable to non-bash shells)
+
+apt update
+apt upgrade
+echo ""
+
+interactive_install_frm_arr() {
+    # $@ - array of packages
+    package_list=("$@")
+
+    while true; do
+        echo "This package list contains the following"
+
+        for id in "${!package_list[@]}"; do    # The ! in ${!package_list[@]} lets us iterate through the keys instead of values (note that all arrays in Bash are associative arrays)
+            package="${package_list[${id}]}"
+            echo "  [${id}] ${package}"
+        done
+
+        read -r -p "Install / Remove [y/N/0-9+]? " choice
+        if [[ "${choice}" =~ ^([yY][eE][sS]|[yY])$ ]]; then     # Non-POSIX compliant
+            for package in ${package_list[@]}; do
+                echo "[[Installing ${package}]]"
+                apt -y install "${package}"
+                echo ""
+            done
+            break
+        elif [[ "${choice}" =~ ^([0-9]+)$ ]]; then
+            unset package_list["${choice}"]    # We simply unset this element (it becomes null and is ignored when iterating)
+        elif [[ "${choice}" = "" ]] || [[ "${choice}" =~ ^([nN][oO]|[nN])$ ]]; then
+            break
+        fi
+        echo ""
+    done
+}
 
 # Compilers / Interpreteres
-apt install gcc
+compilers_packages=("gcc" "python3")
+echo "[COMPILERS / INTERPRETERS]"
+interactive_install_frm_arr "${compilers_packages[@]}"
 
 # Common Utilities
-apt install unzip   # Required for installing certain LSPs in NeoVim's Mason
+utilities_packages=("unzip" "pv")
+echo "[COMMON UTILITIES]"
+interactive_install_frm_arr "${utilities_packages[@]}"
 
 # Stupid Stuff
-apt install neofetch
-apt install lolcat
+stupid_packages=("neofetch" "lolcat" "cowsay" "fortune" "espeak" "pv" "cbonsai")
+echo "[STUPID PACKAGES]"
+interactive_install_frm_arr "${stupid_packages[@]}"
+
