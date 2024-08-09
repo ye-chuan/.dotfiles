3d19e7d725f170721f33d9f0ca841d3af656a076
ye-chuan <76342787+ye-chuan@users.noreply.github.com>
2024-08-10 01:21:23 +0800

    NeoVim: Added `gr` shortcut in LSP config to go to references

--- .config/nvim/after/plugin/lsp-stuff.lua
+++ .config/nvim/after/plugin/lsp-stuff.lua
@@ -155,20 +155,21 @@ vim.api.nvim_create_autocmd("LspAttach", {      -- Attach this autocmd to the bu
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }    -- Store current buffer no. (ie. 0) so mappings are only set for this buffer
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, opts)
     -- vim.keymap.set("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder, opts)
     -- vim.keymap.set("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
     -- vim.keymap.set("n", "<Leader>wl", function()
     --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     -- end, opts)
-    vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     -- vim.keymap.set("n", "<Leader>f", function()
     --   vim.lsp.buf.format { async = true }
     -- end, opts)
