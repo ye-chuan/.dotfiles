506757349950650c3146fd96e51e479c0dbb0181
ye-chuan <76342787+ye-chuan@users.noreply.github.com>
2026-01-04 13:05:29 +0800

    Neovim: Added hack to force OSC52 over SSH sessions

--- .config/nvim/lua/nvimrc.lua
+++ .config/nvim/lua/nvimrc.lua
@@ -2,3 +2,9 @@
 
 vim.opt.winborder = "rounded"   -- Since v0.11: cleans a lot of the LSP handler-specific border configs with this global option
 
+-- HACK: Since tmux < 3.2 do not support `load-buffer -w -` which emits OSC 52, we are temporarily having Neovim directly emit
+-- OSC 52 sequence when in an SSH session (Neovim cannot auto-determine if terminal has support for OSC 52 if the terminal is tmux)
+-- So we are assuming if using NeoVim in a SSH session that the terminal emulator has OSC 52 support.
+if os.getenv("SSH_TTY") then    -- Very simple (not robust) check for SSH Session
+    vim.g.clipboard = "osc52"
+end
