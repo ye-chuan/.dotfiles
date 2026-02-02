a5978ad98e31a21f57acbb3681ae289b2c1a34d7
ye-chuan <76342787+ye-chuan@users.noreply.github.com>
2026-02-03 03:04:34 +0800

    Changed Terminal To Kitty

--- .config/environment/graphical.sh
+++ .config/environment/graphical.sh
@@ -1,5 +1,5 @@
 # Environmental Variables for graphical sessions (e.g. not from SSH or TTY)
 
-export TERMINAL=wezterm
+export TERMINAL=kitty
 export QT_QPA_PLATFORMTHEME=qt5ct:qt6ct
 export ELECTRON_OZONE_PLATFORM_HINT=auto    # To get Electron apps to run natively in Wayland (as of 2025)
